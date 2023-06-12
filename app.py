0ed074e9558efe11f5c259cdef7356f326af13d3
danielavdalian <avdaliansfdk@gmail.com>
2023-06-13 00:26:39 +0200

    feature(tributos):Added new models and made 2 endpoints

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_cors import CORS
-
+from resources.tributo import blp as TributoBlueprint
 from dotenv import load_dotenv
 
 
@@ -17,7 +17,7 @@ CORS(app)
 
 
 app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "pokemon-league API"
+app.config["API_TITLE"] = "Aventura 2023"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
@@ -25,11 +25,11 @@ app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL","sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
+
 migrate = Migrate(app, db)
 
 api = Api(app)
 
-#api.register_blueprint(PlayerBlueprint)
-#api.register_blueprint(MatchBlueprint)
-#api.register_blueprint(JornadaBlueprint)
+api.register_blueprint(TributoBlueprint)
--- models/tributo.py
+++ models/tributo.py
@@ -5,4 +5,6 @@ class TributoModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-   
+    district = db.Column(db.Integer, nullable=False)
+    total_points = db.Column(db.Integer, nullable=False, default = 0)
+    img_src = db.Column(db.String(80), unique=True, nullable=False)
