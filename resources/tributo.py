adaf6bd21571937a8eb2b94b3a686d0bc9919ed0
danielavdalian <avdaliansfdk@gmail.com>
2023-06-23 10:24:06 +0200

    feature(AventuraAPI): Upload file and get
    Now, when we upload a file, we can set the id of the Tributo. We can get with another endpoint the image

--- resources/tributo.py
+++ resources/tributo.py
@@ -1,18 +1,16 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from flask import Flask, flash, request, redirect, url_for, current_app
+from flask import Flask, flash, request, redirect, url_for, current_app, send_file
 from werkzeug.utils import secure_filename
 import os
 
 from db import db
 from models import TributoModel
 
-from schemas import GetTributosSchema, PostTributosSchema
+from schemas import GetTributosSchema,FileSchema,PostTributosSchema
 from marshmallow import Schema, fields
 
-class FileSchema(Schema):
-    file = fields.Field(required=True, type="file", description="Archivo a subir")
-    custom_filename = fields.Str()
+
 
 blp = Blueprint("Tributo", __name__, description="Tributo operations") #swagger
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -22,7 +20,18 @@ class Tributos(MethodView):
     @blp.response(200, GetTributosSchema(many=True))
     def get(self):
         return TributoModel.query.order_by(TributoModel.total_points)
+    
+@blp.route("/Tributo/<int:id>")
+class TributoById(MethodView):
+    @blp.response(200, GetTributosSchema(many=False))
+    def get(self, id):
+        tributo = TributoModel.query.get(id)
+        if tributo is None:
+            return {"message": "Tributo not found"}, 404
+        else:
+            return tributo
 
+    
 @blp.route("/tributo")
 class AddTributo(MethodView):
     @blp.arguments(PostTributosSchema)
@@ -45,12 +54,15 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @blp.route("/upload", methods=["POST"])
 class UploadImage(MethodView):
     @blp.arguments(FileSchema, location="files")
     @blp.response(201)
     def post(self, file_data):
         if file_data:
+            current_app.logger.info(file_data)
+            current_app.logger.info(request.form['id'])
             # check if the post request has the file part
             if 'file' not in request.files:
                 flash('No file part')
@@ -63,7 +75,8 @@ class UploadImage(MethodView):
                 return redirect(request.url)
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                custom_filename = request.form['custom_filename']  # Obtener el valor del campo custom_filename desde el argumento file_data
+                custom_filename = request.form['custom_filename']
+                id = request.form['id']
                 if custom_filename:
                     extension = os.path.splitext(filename)[1]
                     filename = secure_filename(custom_filename) + extension  # Usar el valor custom_filename como nombre de archivo
@@ -72,10 +85,23 @@ class UploadImage(MethodView):
                     abort(409, message="El archivo ya existe.")
                 os.makedirs(directory, exist_ok=True)
                 file.save(os.path.join(directory, filename))
-
+                id_value = request.form['id']
+                tributo = TributoModel.query.get(id_value)
+                tributo.img_src = filename
+                db.session.commit()
                 return {"msg": "Image uploaded"}, 201
             else:
                 flash('Archivo no permitido')
                 return redirect(request.url)
         else:
             abort(400, message="No se proporcionó ningún archivo")
+
+
+@blp.route('/images/<image_name>')
+def get_image(image_name):
+    # Ruta de la carpeta donde se encuentran las imágenes
+    image_folder = current_app.config['UPLOAD_FOLDER']
+    try:
+        return send_file(f"{image_folder}/{image_name}", mimetype='image/jpeg')
+    except FileNotFoundError:
+        return 'Imagen no encontrada', 404
--- schemas.py
+++ schemas.py
@@ -1,16 +1,29 @@
 from marshmallow import Schema, fields, validate
 
+class FileSchema(Schema):
+    file = fields.Field(required=True, type="file", description="Archivo a subir")
+    custom_filename = fields.Str()
+    id = fields.Str()
 
 class GetTributosSchema(Schema):
+
+    id = fields.Int()
     name = fields.Str()
     district = fields.Int()
     total_points = fields.Int()
     img_src = fields.Str()
+
+class GetTributosIdSchema(Schema):
+    id = fields.Int()
 
 class PostTributosSchema(Schema):
     name = fields.Str()
     district = fields.Int()
     total_points = fields.Int()
     img_src = fields.Str()
+    
+
+
+
 
 
